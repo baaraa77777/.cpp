10fecbc1d26dbce57874f9e706bf4f031c934ad9
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-08-15 14:21:02 +0545

    Create 2.cpp

---
+++ Lab Work/2.cpp
@@ -0,0 +1,14 @@
+//2) Write a program that uses a for loop to output the numbers from 1 to 10.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "  ";
+    }
+    
+    return 0;
+}
