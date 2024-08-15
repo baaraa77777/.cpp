52a5f93c5cc7eb3e7e2074b1046c49e6b5f33dd8
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-08-15 14:37:09 +0545

    create 7.cpp

--- lab work/7.cpp
+++ lab work/7.cpp
@@ -1,3 +1,7 @@
+//7) Write a program that demonstrates function overloading by defining twofunctions with the same name but different
+//parameter lists. One function should take two integers as arguments and return their sum, and the other function 
+//should take three integers as arguments and return their sum. Call both functions in the main () function and print the results.
+
 #include <iostream>
 using namespace std;
 int sum(int a, int b)
