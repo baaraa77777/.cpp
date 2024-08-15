173f17fb7663105b8d045f8a2fcdc8b8295faac8
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-08-15 14:37:59 +0545

    Create 9.cpp

--- lab work/9.cpp
+++ lab work/9.cpp
@@ -1,3 +1,6 @@
+//9) Write a program that demonstrates the use of the different storage classes in C++ (local, global, static, and extern). 
+//Define a variable with each storage class and print the value of the variable in the main () function.
+
 #include <iostream>
 
 using namespace std;
