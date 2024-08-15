4b62f05524327eb69516ce2815d381e4c0d2895b
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-08-15 14:38:53 +0545

    create 8.cpp

--- lab work/8.cpp
+++ lab work/8.cpp
@@ -1,3 +1,6 @@
+//8) Write a program that defines a function that takes two arguments: an integer and a string. The function should have a default 
+//argument for the string, so that if it is not provided when the function is called, the default value "Default String" will be used.
+
 #include <iostream>
 using namespace std;
 void functio(int a, string b = "Default String")
