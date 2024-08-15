1d4b87f1d4c0039019bf9c93e6b4a36c536de364
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-08-15 16:03:07 +0545

    Create26.cpp

--- lab work/26.cpp
+++ lab work/26.cpp
@@ -1,7 +1,25 @@
-//26.Write a conversion routine in c++ that can convert user-defined datadistance to basic data 
-//float. Assume that the class distance contains twodata members (feet (integer type) and inch 
-//(floating point type). NOTE 1- meter = 3.33 feet and 1 feet = 12 inches)
+//26.Write a conversion routine in c++ that can convert user-defined datadistance to basic data float. Assume that the class distance 
+//contains twodata members (feet (integer type) and inch (floating point type). NOTE 1- meter = 3.33 feet and 1 feet = 12 inches).
 
 #include<iostream>
 using namespace std;
 
+class Distance {
+    int feet;
+    float inch;
+    public:
+    Distance(int f, float i){
+        feet=f;
+        inch=i;
+    }
+    operator float(){
+        float ft=feet + inch/12;
+        return (ft/3.28);
+    }
+};
+int main(){
+    Distance d1(20,5.9);
+    float meter=d1;
+    cout<<"The given distance in meters is: "<<meter<<endl;
+    return 0;
+}
